9edfd22f1693849646b03ba44b9fa89b17d1b593
Mikhail L <rillde52@mail.ru>
2025-11-24 02:07:16 +0700

    rep init

---
+++ restful-booker/src/test/java/restful/booker/tests/AbstractTest.java
@@ -0,0 +1,17 @@
+package restful.booker.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringBootConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+@SpringBootConfiguration
+public class AbstractTest {
+
+    @Test
+    public void firstTest() {
+        assertTrue(true);
+    }
+}
---
+++ restful-booker/src/test/java/restful/booker/tests/config/Rest.java
@@ -0,0 +1,15 @@
+package restful.booker.tests.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@Configuration
+public class Rest {
+
+    @Bean
+    public WebTestClient restClient() {
+        WebTestClient client = WebTestClient.bindToServer().baseUrl("https://restful-booker.herokuapp.com").build();
+        return client;
+    }
+}
